7b252b9223af99104d2dda6d1736e07e7fda0a6d
Menna Reda <95769532+Mennaallahreda25@users.noreply.github.com>
2022-12-02 04:36:43 +0200

    Add files via upload

---
+++ task 4/myfirlib.py
@@ -0,0 +1,14 @@
+a=20
+b=30
+c=b-a
+
+def printusername(i):
+    print(i)
+
+def max2(x,y):
+    if x>y:
+        return x
+    else:
+       return y
+
+mylist=["python", "c++", "java", "ruby", "R"]
