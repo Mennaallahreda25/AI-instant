0288f7d50a33dfd4eea004c401441ef2a17fbec5
Menna Reda <95769532+Mennaallahreda25@users.noreply.github.com>
2022-12-02 04:46:38 +0200

    Add files via upload

---
+++ task 9/information_f.py
@@ -0,0 +1,22 @@
+clientinfo='''
+Ahmed Mohamed Sabaan Ahmed123@yahoo.com 01122727762
+Bassant Sayed Hasaan Bassant-2@mywork.net 01033484420
+Amira Hassan Mostafa Amira4@uni.edu 01222789906
+Menna Allah Reda Menna239@gmail.com 010022927782
+Radwa Mohamed Samy Radwa@gmail.com 010038936642
+Mariam Hussan Samy Mariam66@universty.edu 01522865543
+Mahmoud Mohamed Samy Mahmoud.2@yahoo.com 01122334455
+Donia Samy Mohamed Doniasamy@uni.org 010033556633
+Sara Mohamed Elshamy Sara77@uni.org 01244228899
+Eslam Zenhom Gamel Esalm@gmail.com 01133448899
+Gamal Samy ZEnhom Gamal.222@up.net 01099863328
+Ahmed Samy Mostafa Ahmed@gmail.com 01133223322
+Ahmed Mohamed Gamel AH345@gmail.com 01001122334
+Ahmed Fayez Sabaan AHF.2@yahoo.com 01122927765
+Ahmed Lotfy Samy lot@up.net 01122990077
+Ahmed Mahoud Raby Raby88@kids.net 01566778899
+Mohamed Hussan Sayed Mohamed.h@yahoo.com 01244556677
+Mohamed Mohamed Elshamy Mohamed222@yahoo.com 01244779900
+Donia Sayed Mahfoz Donia@mywork.org 01522334455
+Sara Hafez Shawky Sara22.2@yahoo.com 01255448899
+'''
